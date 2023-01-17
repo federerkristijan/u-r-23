75078e83a5abb9fa193b8a8bd1b91733b31127ef
federerkristijan <federer.kristijan@gmail.com>
2023-01-17 20:15:42 +0100

    news/index.js -> Links created

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 const NewsPage = () => {
   return (
-    <div>The News Page</div>
-  )
-}
+    <Fragment>
+      <div>The News Page</div>
+      <ul>
+        <li>
+          <Link>News 1</Link>
+        </li>
+        <li>
+          <Link>News 2</Link>
+        </li>
+        <li>
+          <Link>News 3</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+};
 
-export default NewsPage
+export default NewsPage;
