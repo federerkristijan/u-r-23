1fbab5358d3c7fc9bb2d36437736c4c777a82585
federerkristijan <federer.kristijan@gmail.com>
2023-01-18 21:13:07 +0100

    output of a lsit of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
-const HomePage = () => {
+import MeetupList from "components/meetups/MeetupList";
+
+const dummy_meetups = [
+  {
+    id: 'm1',
+    title: 'The 1st meetup',
+    image: 'https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg',
+    address: 'S Wannsee',
+    description: 'swimming'
+  },
+  {
+    id: 'm2',
+    title: 'The 2nd meetup',
+    image: 'https://withberlinlove.com/wp-content/uploads/2013/06/Strandbad-Pl%c3%b6tzensee.jpg',
+    address: 'Plötzensee',
+    description: 'swimming closer'
+  },
+]
+
+const HomePage = (props) => {
   return (
-    <div>The Home Page</div>
+    <MeetupList meetups={dummy_meetups}/>
   )
 }
 
