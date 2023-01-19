02ec8d5aabe967093e9f0a870b62ebb4e9f177aa
federerkristijan <federer.kristijan@gmail.com>
2023-01-19 11:48:30 +0100

    changed link to -> href

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
+import Layout from "components/layout/Layout";
 import MeetupList from "components/meetups/MeetupList";
 
 const dummy_meetups = [
   {
-    id: 'm1',
-    title: 'The 1st meetup',
-    image: 'https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg',
-    address: 'S Wannsee',
-    description: 'swimming'
+    id: "m1",
+    title: "The 1st meetup",
+    image:
+      "https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg",
+    address: "S Wannsee",
+    description: "swimming",
   },
   {
-    id: 'm2',
-    title: 'The 2nd meetup',
-    image: 'https://withberlinlove.com/wp-content/uploads/2013/06/Strandbad-Pl%c3%b6tzensee.jpg',
-    address: 'Plötzensee',
-    description: 'swimming closer'
+    id: "m2",
+    title: "The 2nd meetup",
+    image:
+      "https://withberlinlove.com/wp-content/uploads/2013/06/Strandbad-Pl%c3%b6tzensee.jpg",
+    address: "Plötzensee",
+    description: "swimming closer",
   },
-]
+];
 
 const HomePage = (props) => {
   return (
-    <MeetupList meetups={dummy_meetups}/>
-  )
-}
+    <Layout>
+      <MeetupList meetups={dummy_meetups} />
+    </Layout>
+  );
+};
 
 export default HomePage;
