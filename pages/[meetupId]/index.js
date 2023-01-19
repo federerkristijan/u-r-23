eb36e6eb28dc346ee764dc1f42fd123181dcdca8
federerkristijan <federer.kristijan@gmail.com>
2023-01-19 13:06:47 +0100

    img instead of Image - problems with props

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { Fragment } from 'react'
 
 const MeetupDetail = () => {
   return (
-    <div>MeetupDetail</div>
+    <Fragment>
+      {/* <Image src='https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg' width={220} height={180} alt='1st meetup' /> */}
+      <img src='https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg' alt='1st meeting' />
+      <h1>Our first meetup</h1>
+      <address>S Wannsee</address>
+      <p>The meetup description</p>
+    </Fragment>
   )
 }
 
