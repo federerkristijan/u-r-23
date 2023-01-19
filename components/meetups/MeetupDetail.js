79e267f596bd4ffb1472b19aa3ed15acd25e8ac9
federerkristijan <federer.kristijan@gmail.com>
2023-01-19 13:09:40 +0100

    MeetupDetail.js created and rendered into MeetupDetailS

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+const MeetupDetail = (props) => {
+  return (
+    <Fragment>
+      {/* <Image src='https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg' width={220} height={180} alt='1st meetup' /> */}
+      <img src='https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg' alt='1st meeting' />
+      <h1>Our first meetup</h1>
+      <address>S Wannsee</address>
+      <p>The meetup description</p>
+    </Fragment>
+  )
+}
+
+export default MeetupDetail
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,9 @@
-import Image from 'next/image'
-import React, { Fragment } from 'react'
+import MeetupDetail from "components/meetups/MeetupDetail"
 
-const MeetupDetail = () => {
+const MeetupDetails = () => {
   return (
-    <Fragment>
-      {/* <Image src='https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg' width={220} height={180} alt='1st meetup' /> */}
-      <img src='https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg' alt='1st meeting' />
-      <h1>Our first meetup</h1>
-      <address>S Wannsee</address>
-      <p>The meetup description</p>
-    </Fragment>
+    <MeetupDetail />
   )
 }
 
-export default MeetupDetail
+export default MeetupDetails
