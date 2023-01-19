1d0f95d8c6621664a6b94084768f63b49bf23111
federerkristijan <federer.kristijan@gmail.com>
2023-01-19 13:23:59 +0100

    meetupdetail.module.css added

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,14 +1,16 @@
 import { Fragment } from "react"
 
+import classes from "MeetupDetail.module.css"
+
 const MeetupDetail = (props) => {
   return (
-    <Fragment>
+    <section className={classes.detail}>
       {/* <Image src='https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg' width={220} height={180} alt='1st meetup' /> */}
       <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
-    </Fragment>
+    </section>
   )
 }
 
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,14 @@
-import MeetupDetail from "components/meetups/MeetupDetail"
+import MeetupDetail from "components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
   return (
-    <MeetupDetail />
-  )
-}
+    <MeetupDetail
+      image="https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg"
+      title="1st Meetup"
+      address="S Wannsee"
+      description="this is 1st meetup"
+    />
+  );
+};
 
-export default MeetupDetails
+export default MeetupDetails;
