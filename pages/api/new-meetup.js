c2125b32198510b90a6826a0d8ae79d040c176ac
federerkristijan <federer.kristijan@gmail.com>
2023-01-20 19:28:48 +0100

    mongoDb conntect

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,14 @@
+import { MongoClient } from "mongodb";
+
+const password = env.process.NEXT_APP_MONGODB_PASSWORD;
+
 const handler = (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
 
     const { title, image, address, description } = data;
 
-    
+    MongoClient.connect('mongodb+srv://kfederer:{password}@cluster0.xa99u.mongodb.net/meetups?retryWrites=true&w=majority');
   }
 }
 
