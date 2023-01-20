eca74fafb53d9464b6a5191fc78c0f7751f59de1
federerkristijan <federer.kristijan@gmail.com>
2023-01-20 17:24:36 +0100

    getStaticProps in a dynamci page [meetupId]

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,18 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticProps = async() => {
+  return {
+    props: {
+      meetupData: {
+        image: "https://www.berlinerbaeder.de/fileadmin/_processed_/3/4/csm_5_strandbad_wannsee_1840x860px_428ed79620.jpg",
+        id: 'm1',
+        title: "1st Meetup",
+        address: 'S Wannsee',
+        description: "this is 1st meetup"
+      }
+    }
+  }
+};
+
 export default MeetupDetails;
