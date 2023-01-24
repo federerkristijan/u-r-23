ddf512898ec4874996a12dd03d728dbe8be7b65e
federerkristijan <federer.kristijan@gmail.com>
2023-01-24 16:13:21 +0100

    added head to the pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import Head from "next/head";
 import { MongoClient } from "mongodb";
+import { Fragment } from "react";
 
 import MeetupList from "components/meetups/MeetupList";
 
-
-
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return;
+  <Fragment>
+    <Head></Head>
+    <MeetupList meetups={props.meetups} />;
+  </Fragment>;
 };
 
 // executed on the server on every request
@@ -20,16 +24,14 @@ const HomePage = (props) => {
 //   }
 // }
 
-
 // exectuted during the build process, never shown on client-side
-export const getStaticProps = async() => {
-
+export const getStaticProps = async () => {
   const client = await MongoClient.connect(
     "mongodb+srv://kfederer:Xt8NhMqeQAsByX7Y@cluster0.7l67fin.mongodb.net/?retryWrites=true&w=majority"
   );
   const db = client.db();
 
-  const meetupsCollection = db.collection('meetups');
+  const meetupsCollection = db.collection("meetups");
 
   const meetups = await meetupsCollection.find().toArray();
 
@@ -37,15 +39,15 @@ export const getStaticProps = async() => {
 
   return {
     props: {
-      meetups: meetups.map(meetup => ({
+      meetups: meetups.map((meetup) => ({
         title: meetup.title,
         address: meetup.address,
         image: meetup.image,
-        id: meetup._id.toString()
-      }))
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 10
-  }
-}
+    revalidate: 10,
+  };
+};
 
 export default HomePage;
